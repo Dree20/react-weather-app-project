ca4085f51bd0eaddd9565102a684e4659e548cb9
Dree20 <dreannaad@gmail.com>
2025-12-29 18:53:25 -0500

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This projetc was coded by{" "}
+        <a
+          href="https://github.com/Dree20?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Dreanna
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Dree20/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Github{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
