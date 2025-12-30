846eb8422f67e0c8105010418bbde159c30c2207
Dree20 <dreannaad@gmail.com>
2025-12-30 12:22:43 -0500

    Added Bootstrap to index.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This projetc was coded by{" "}
-        <a
-          href="https://github.com/Dree20?tab=repositories"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Dreanna
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Dree20/react-weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on Github{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This projetc was coded by{" "}
+          <a
+            href="https://github.com/Dree20?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Dreanna
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Dree20/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
