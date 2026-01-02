4aa93695cfedaa5d1000887418ca419737625ce1
Dree20 <dreannaad@gmail.com>
2026-01-02 12:59:04 -0500

    Added map to WeatherForecast.js to display the next five days.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,15 +7,6 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
-  useEffect(() => {
-    setLoaded(false);
-    const apiKey = "babdtd562ab541fbc49o01aca733b4ff ";
-    let city = props.city;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-  }, [props.city]);
-
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
@@ -26,13 +17,25 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
   } else {
+    const apiKey = "babdtd562ab541fbc49o01aca733b4ff ";
+    let city = props.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
     return null;
   }
 }
